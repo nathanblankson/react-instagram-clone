b128d48cf1cb29e4da283a278d2763f10e30149b
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-03 15:42:59 +0100

    feat: add theme for chakra

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,29 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, type StyleFunctionProps } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const styles = {
+    global: (props: StyleFunctionProps) => ({
+        body: {
+            bg: mode('gray.100', '#000')(props),
+            color: mode('gray.800', 'whiteAlpha.900')(props),
+        },
+    }),
+}
+
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config, styles })
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <App/>
         </ChakraProvider>
     </StrictMode>,
