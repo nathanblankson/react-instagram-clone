1151f364f09cbb3594ca78ac3ca6d72a226f89ad
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-10 16:55:39 +0100

    feat: add get feed posts hook

---
+++ src/hooks/useGetFeedPosts.tsx
@@ -0,0 +1,61 @@
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { firestore } from '../firebase/firebase';
+import useAuthStore from '../store/authStore';
+import usePostStore, { type BasePost, type Post } from '../store/postStore';
+import useUserProfileStore from '../store/userProfileStore';
+import useShowToast from './useShowToast';
+
+const useGetFeedPosts = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const { posts, setPosts } = usePostStore();
+    const authUser = useAuthStore((state) => state.authUser);
+    const showToast = useShowToast();
+    const { setUserProfile } = useUserProfileStore();
+
+    useEffect(() => {
+        const getFeedPosts = async () => {
+            if (!authUser) {
+                return;
+            }
+
+            setIsLoading(true);
+
+
+            if (authUser.following.length === 0) {
+                setIsLoading(false);
+                setPosts([]);
+                return;
+            }
+            const q = query(collection(firestore, 'posts'), where('createdBy', 'in', authUser.following));
+            try {
+                const querySnapshot = await getDocs(q);
+                const feedPosts: Post[] = [];
+
+                querySnapshot.forEach((doc) => {
+                    feedPosts.push({ id: doc.id, ...doc.data() as BasePost });
+                });
+
+                feedPosts.sort((a, b) => b.createdAt - a.createdAt);
+
+                setPosts(feedPosts);
+            } catch (error) {
+                if (error instanceof Error) {
+                    showToast('Error', error.message, 'error');
+                } else {
+                    showToast('Error', 'An unknown error occurred', 'error');
+                }
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        if (authUser) {
+            void getFeedPosts();
+        }
+    }, [authUser, showToast, setPosts, setUserProfile]);
+
+    return { isLoading, posts };
+};
+
+export default useGetFeedPosts;
