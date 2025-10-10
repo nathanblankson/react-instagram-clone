f8b3410055d049875ee16b7e49eedfd3e7ece2c4
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-10 13:29:54 +0100

    feat: add link to profile page in suggested users header

--- src/components/SuggestedUsers/SuggestedUsersHeader.tsx
+++ src/components/SuggestedUsers/SuggestedUsersHeader.tsx
@@ -1,16 +1,27 @@
 import { Avatar, Button, Flex, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import useLogout from '../../hooks/useLogout.tsx';
+import useAuthStore from '../../store/authStore.tsx';
 
 export default function SuggestedUsersHeader() {
     const { handleLogout, isLoggingOut } = useLogout();
+    const authUser = useAuthStore((state) => state.authUser);
+
+    if (!authUser) {
+        return null;
+    }
 
     return (
         <Flex justifyContent={'space-between'} alignItems={'center'} w={'full'}>
             <Flex alignItems={'center'} gap={2}>
-                <Avatar src="/profilepic.png" name="nathanblankson" size={'lg'}/>
-                <Text fontSize={12} fontWeight={'bold'}>
-                    nathanblankson
-                </Text>
+                <Link to={authUser.username}>
+                    <Avatar src={authUser.profilePicURL} size={'lg'}/>
+                </Link>
+                <Link to={authUser.username}>
+                    <Text fontSize={12} fontWeight={'bold'}>
+                        {authUser.username}
+                    </Text>
+                </Link>
             </Flex>
             <Button
                 size={'xs'}
