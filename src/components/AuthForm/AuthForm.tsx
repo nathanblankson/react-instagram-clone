513b6bfab733c53ad717dff80510e98ce698737a
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-10 13:32:05 +0100

    refactor: add prefix to google auth

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -22,7 +22,7 @@ export default function AuthForm() {
                         <Box flex={2} h={'1px'} bg={'gray.400'}/>
                     </Flex>
 
-                    <GoogleAuth/>
+                    <GoogleAuth prefix={isLogin ? 'Log in' : 'Sign up'}/>
                 </VStack>
             </Box>
 
--- src/components/AuthForm/GoogleAuth.tsx
+++ src/components/AuthForm/GoogleAuth.tsx
@@ -1,11 +1,17 @@
 import { Flex, Image, Text } from '@chakra-ui/react';
 
-export default function GoogleAuth() {
+export interface GoogleAuthProps {
+    prefix: string;
+}
+
+export default function GoogleAuth(
+    { prefix }: GoogleAuthProps,
+) {
     return (
         <Flex alignItems={'center'} justifyContent={'center'} cursor={'pointer'}>
             <Image src="/google.png" alt="Google Logo" w={5}/>
             <Text mx={2} color={'blue.500'}>
-                Log in with Google
+                {prefix} with Google
             </Text>
         </Flex>
     )
