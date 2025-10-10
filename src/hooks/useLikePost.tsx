28adde8d5d816ef5877320e0da1c307f4ca03cc1
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-10 17:46:09 +0100

    fix: update use like post

--- src/hooks/useLikePost.tsx
+++ src/hooks/useLikePost.tsx
@@ -7,15 +7,12 @@ import useShowToast from './useShowToast';
 
 const useLikePost = (post: Post) => {
     const [isUpdating, setIsUpdating] = useState(false);
+
     const authUser = useAuthStore((state) => state.authUser);
 
-    // TODO: NBSon - look at how best to handle these when using in other hooks
-    if (!authUser) {
-        return;
-    }
-
     const [likes, setLikes] = useState(post.likes.length);
-    const [isLiked, setIsLiked] = useState(post.likes.includes(authUser.uid));
+    const [isLiked, setIsLiked] = useState(post.likes.includes(authUser?.uid || '')); // TODO: see how to better handle this
+
     const showToast = useShowToast();
 
     const handleLikePost = async () => {
@@ -37,7 +34,7 @@ const useLikePost = (post: Post) => {
             });
 
             setIsLiked(!isLiked);
-            
+
             isLiked ? setLikes(likes - 1) : setLikes(likes + 1);
         } catch (error) {
             if (error instanceof Error) {
