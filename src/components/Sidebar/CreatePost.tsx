44e6a4ff760dbe0154c12ec997b76b85e9318763
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-10 16:47:47 +0100

    feat: hook up create post

--- src/components/Sidebar/CreatePost.tsx
+++ src/components/Sidebar/CreatePost.tsx
@@ -1,7 +1,51 @@
-import { Box, Flex, Tooltip } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    CloseButton,
+    Flex,
+    Image,
+    Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Textarea,
+    Tooltip,
+    useDisclosure,
+} from '@chakra-ui/react';
+import { useRef, useState } from 'react';
+import { BsFillImageFill } from 'react-icons/bs';
 import { CreatePostLogo } from '../../assets/constants';
+import useCreatePost from '../../hooks/useCreatePost.tsx';
+import usePreviewImage from '../../hooks/usePreviewImage.tsx';
+import useShowToast from '../../hooks/useShowToast';
 
 const CreatePost = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [caption, setCaption] = useState('');
+    const imageRef = useRef<HTMLInputElement | null>(null);
+    const { handleImageChange, selectedFile, setSelectedFile } = usePreviewImage();
+    const showToast = useShowToast();
+    const { isLoading, handleCreatePost } = useCreatePost();
+
+    const handlePostCreation = async () => {
+        try {
+            await handleCreatePost(selectedFile, caption);
+            onClose();
+            setCaption('');
+            setSelectedFile(null);
+        } catch (error) {
+            if (error instanceof Error) {
+                showToast('Error', error.message, 'error');
+            } else {
+                showToast('Error', 'An unexpected error occurred', 'error');
+            }
+        }
+    };
+
     return (
         <>
             <Tooltip
@@ -20,11 +64,55 @@ const CreatePost = () => {
                     p={2}
                     w={{ base: 10, md: 'full' }}
                     justifyContent={{ base: 'center', md: 'flex-start' }}
+                    onClick={onOpen}
                 >
                     <CreatePostLogo/>
                     <Box display={{ base: 'none', md: 'block' }}>Create</Box>
                 </Flex>
             </Tooltip>
+
+            <Modal isOpen={isOpen} onClose={onClose} size="xl">
+                <ModalOverlay/>
+
+                <ModalContent bg={'black'} border={'1px solid gray'}>
+                    <ModalHeader>Create Post</ModalHeader>
+                    <ModalCloseButton/>
+                    <ModalBody pb={6}>
+                        <Textarea
+                            placeholder="Post caption..."
+                            value={caption}
+                            onChange={(e) => setCaption(e.target.value)}
+                        />
+
+                        <Input type="file" hidden ref={imageRef} onChange={handleImageChange}/>
+
+                        <BsFillImageFill
+                            onClick={() => imageRef.current?.click()}
+                            style={{ marginTop: '15px', marginLeft: '5px', cursor: 'pointer' }}
+                            size={16}
+                        />
+                        {selectedFile && (
+                            <Flex mt={5} w={'full'} position={'relative'} justifyContent={'center'}>
+                                <Image src={selectedFile} alt="Selected img"/>
+                                <CloseButton
+                                    position={'absolute'}
+                                    top={2}
+                                    right={2}
+                                    onClick={() => {
+                                        setSelectedFile(null);
+                                    }}
+                                />
+                            </Flex>
+                        )}
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button mr={3} onClick={handlePostCreation} isLoading={isLoading}>
+                            Post
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     );
 };
