1ce235a6518de63ac68fc64333f860701e1ad2ab
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-10 14:37:43 +0100

    refactor: add interface to user in profile state

--- src/store/userProfileStore.tsx
+++ src/store/userProfileStore.tsx
@@ -1,7 +1,8 @@
 import { create } from 'zustand';
+import type { User } from './authStore.tsx';
 
 interface UserProfileState {
-    userProfile: any;
+    userProfile: User | null;
     setUserProfile: (profile: any) => void;
     // addPost: (post: any) => void;
 }
