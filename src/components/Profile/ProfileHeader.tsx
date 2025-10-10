61e3ff212e44f3c5de7758e70835a2e2b9b390d2
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-10 14:38:44 +0100

    feat: conditionally show profile header buttons depending on user being viewed

--- src/components/Profile/ProfileHeader.tsx
+++ src/components/Profile/ProfileHeader.tsx
@@ -1,8 +1,19 @@
 import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from '@chakra-ui/react';
 import { useState } from 'react';
+import useAuthStore from '../../store/authStore.tsx';
+import useUserProfileStore from '../../store/userProfileStore.tsx';
 
 export default function ProfileHeader() {
     const [isFollowing] = useState(false);
+    const authUser = useAuthStore(state => state.authUser);
+    const { userProfile } = useUserProfileStore();
+
+    if (!userProfile) {
+        return null; // TODO: NBSon - loading spinner, or some placeholder
+    }
+
+    const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
+    const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
 
     return (
         <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: 'column', sm: 'row' }}>
@@ -11,7 +22,7 @@ export default function ProfileHeader() {
                 justifySelf={'center'}
                 alignSelf={'flex-start'}
                 mx={'auto'}>
-                <Avatar src="/profilepic.png" name="nathanblankson"/>
+                <Avatar src={userProfile.profilePicURL}/>
             </AvatarGroup>
 
             <VStack alignItems={'start'} gap={2} mx={'auto'} flex={1}>
@@ -22,56 +33,62 @@ export default function ProfileHeader() {
                     alignItems={'center'}
                     w={'full'}>
                     <Text fontSize={{ base: 'sm', md: 'lg', }}>
-                        nathanblankson
+                        {userProfile.username}
                     </Text>
 
-                    <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
-                        <Button bg={'white'} color={'black'} _hover={{ bg: 'whiteAlpha.800' }} size={{
-                            base: 'xs',
-                            md: 'sm'
-                        }}>
-                            Edit Profile
-                        </Button>
-                    </Flex>
+                    {visitingOwnProfileAndAuth && (
+                        <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+                            <Button
+                                bg={'white'}
+                                color={'black'}
+                                _hover={{ bg: 'whiteAlpha.800' }}
+                                size={{ base: 'xs', md: 'sm' }}
+                            >
+                                Edit Profile
+                            </Button>
+                        </Flex>
+                    )}
 
-                    <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
-                        <Button
-                            bg={'blue.500'}
-                            color={'white'}
-                            _hover={{ bg: 'blue.600' }}
-                            size={{ base: 'xs', md: 'sm' }}
-                        >
-                            {isFollowing ? 'Unfollow' : 'Follow'}
-                        </Button>
-                    </Flex>
+                    {visitingAnotherProfileAndAuth && (
+                        <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+                            <Button
+                                bg={'blue.500'}
+                                color={'white'}
+                                _hover={{ bg: 'blue.600' }}
+                                size={{ base: 'xs', md: 'sm' }}
+                            >
+                                {isFollowing ? 'Unfollow' : 'Follow'}
+                            </Button>
+                        </Flex>
+                    )}
                 </Flex>
 
                 <Flex alignItems={'center'} gap={{ base: 2, sm: 4 }}>
                     <Text fontSize={{ base: 'xs', md: 'sm' }}>
                         <Text as="span" fontWeight={'bold'} mr={1}>
-                            4
+                            {userProfile.posts.length}
                         </Text>
                         Posts
                     </Text>
                     <Text fontSize={{ base: 'xs', md: 'sm' }}>
                         <Text as="span" fontWeight={'bold'} mr={1}>
-                            149
+                            {userProfile.followers.length}
                         </Text>
                         Followers
                     </Text>
                     <Text fontSize={{ base: 'xs', md: 'sm' }}>
                         <Text as="span" fontWeight={'bold'} mr={1}>
-                            175
+                            {userProfile.following.length}
                         </Text>
                         Following
                     </Text>
                 </Flex>
                 <Flex alignItems={'center'} gap={4}>
                     <Text fontSize={'sm'} fontWeight={'bold'}>
-                        Nathan Blankson
+                        {userProfile.fullName}
                     </Text>
                 </Flex>
-                <Text fontSize={'sm'}>Full Stack Developer in London</Text>
+                <Text fontSize={'sm'}>{userProfile.bio}</Text>
             </VStack>
         </Flex>
     )
